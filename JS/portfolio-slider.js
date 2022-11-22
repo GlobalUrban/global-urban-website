08453f3ea473535f47323c8174412891860e7650
Edgardo Valencia <edgardo.avalencia@gmail.com>
2022-11-22 15:59:26 -0800

    slider added to the portfolio.

--- JS/portfolio-slider.js
+++ JS/portfolio-slider.js
@@ -1,9 +1,17 @@
 const portfolioBtn = document.getElementsByClassName("portfolio-btn");
-const portfolioBtnFocused = document.getElementById("portfolio-focused")
+const portfolioBtnFocused = document.getElementById("portfolio-focused");
+const slider = document.getElementById("slider");
 
-console.log(portfolioBtn);
+
+
+portfolioBtn[0].addEventListener('click', () => {
+    slider.style = `margin-left: 0%`;
+})
+
+
+
 portfolioBtn[1].addEventListener('click', () => {
 portfolioBtnFocused.classList.remove("portfolio-focused")
 portfolioBtnFocused.classList.add("portfolio-color-blue");
-console.log("a")
+slider.style = `margin-left: -100%`;
 })
